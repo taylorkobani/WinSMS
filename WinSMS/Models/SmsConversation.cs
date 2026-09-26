1de7c38f4cf5b10e8c56b889fee21d57fb7c02d1
Taylor Kobani <taylor.kobani@gmail.com>
2026-09-26 11:46:36 +0100

    Make conversation messages observable

--- WinSMS/Models/SmsConversation.cs
+++ WinSMS/Models/SmsConversation.cs
@@ -1,9 +1,11 @@
+using System.Collections.ObjectModel;
+
 namespace WinSMS.Models;
 
 public class SmsConversation
 {
     public string PhoneNumber { get; set; } = string.Empty;
-    public List<SmsMessage> Messages { get; set; } = new();
+    public ObservableCollection<SmsMessage> Messages { get; set; } = new();
     public SmsMessage? LastMessage => Messages.OrderByDescending(m => m.Timestamp).FirstOrDefault();
     public DateTimeOffset LastMessageTimestamp => LastMessage?.Timestamp ?? DateTimeOffset.MinValue;
     public int UnreadCount => Messages.Count(m => m.Direction == SmsDirection.Incoming && !m.IsRead);
