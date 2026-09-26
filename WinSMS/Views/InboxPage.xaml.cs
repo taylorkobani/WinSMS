b9af407c92ec05e3083988695aaa90bee3939d00
Taylor Kobani <taylor.kobani@gmail.com>
2026-09-26 15:19:45 +0100

    Focus reply box and send messages with Enter

--- WinSMS/Views/InboxPage.xaml.cs
+++ WinSMS/Views/InboxPage.xaml.cs
@@ -6,6 +6,8 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
+using Windows.System;
+using Windows.UI.Core;
 using WinSMS.Models;
 using WinSMS.ViewModels;
 
@@ -39,6 +41,38 @@ public sealed partial class InboxPage : Page
         {
             ObserveSelectedConversation();
             ScrollConversationToEnd();
+            FocusReplyTextBox();
+        }
+    }
+
+    private void FocusReplyTextBox()
+    {
+        if (ViewModel.SelectedConversation == null)
+            return;
+
+        DispatcherQueue.TryEnqueue(DispatcherQueuePriority.Low, () =>
+        {
+            ReplyTextBox.Focus(FocusState.Programmatic);
+            ReplyTextBox.SelectionStart = ReplyTextBox.Text?.Length ?? 0;
+        });
+    }
+
+    private void ReplyTextBox_KeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+        if (e.Key != VirtualKey.Enter)
+            return;
+
+        var shiftDown =
+            Microsoft.UI.Input.InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Shift)
+                .HasFlag(CoreVirtualKeyStates.Down);
+
+        if (shiftDown)
+            return;
+
+        if (ViewModel.SendReplyCommand.CanExecute(null))
+        {
+            e.Handled = true;
+            ViewModel.SendReplyCommand.Execute(null);
         }
     }
 
